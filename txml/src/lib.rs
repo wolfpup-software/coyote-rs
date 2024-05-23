2ee387818e01b14c6b1f161503770f9e714dd712
Taylor Vann <brian.t.vann@gmail.com>
2024-05-22 22:49:41 -0700

    remove parsley from txml

--- txml/src/lib.rs
+++ txml/src/lib.rs
@@ -1,10 +1,8 @@
 use std::collections::HashMap;
 use std::vec;
 
-use parsley::{parse_str, Step, StepKind};
-
-#[derive(Debug, Clone, Eq, PartialEq)]
 // Components are injected into templates
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Component {
     Text(String),
     AttrVal(String, String),
@@ -13,18 +11,11 @@ pub enum Component {
     None,
 }
 
-// give a more romantic type name for composition
-// "template" is the string, not what is being returned
-// Component is just for names and readability in codebases
-
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Template {
     pub template_str: String,
     pub injections: Vec<Component>,
 }
-
-// ergonomic functions to quickly create Injection Enums
-// (makes component code considerably more readable)
 
 // defacto template function
 pub fn txml<const N: usize>(template_str: &str, injections: [Component; N]) -> Component {
@@ -33,6 +24,9 @@ pub fn txml<const N: usize>(template_str: &str, injections: [Component; N]) -> C
         injections: Vec::from(injections),
     })
 }
+
+// ergonomic functions to quickly create Injection Enums
+// (makes component code considerably more readable)
 
 pub fn text(txt: &str) -> Component {
     Component::Text(txt.to_string())
