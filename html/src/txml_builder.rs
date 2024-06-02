d82fc40f9939f1e024726921d04e92458e41e5ea
Taylor Vann <brian.t.vann@gmail.com>
2024-06-02 12:43:51 -0700

    add parse_str

--- html/src/txml_builder.rs
+++ html/src/txml_builder.rs
@@ -1,4 +1,5 @@
 use parsley::{get_text_from_step, parse_str, Step, StepKind};
+use txml::Template;
 
 // // use these injection details
 // // for static html, we at least need to know the last tag element
@@ -29,57 +30,55 @@ impl HtmlBuilder {
         }
     }
 
-    // pub fn build(&mut self, template: Template) -> HtmlBuilderResults {
-    //     let mut results = HtmlBuilderResults::new();
-    //     let steps = parse_str(&template.template_str);
-    //     for step in steps {
-    //         push_step(results, &template.template_str, step);
-    //     }
+    pub fn build(&mut self, template: Template) -> HtmlBuilderResults {
+        let mut results = HtmlBuilderResults::new();
 
-    //     // pass template and results and step
+        for step in parse_str(&template.template_str, StepKind::Initial) {
+            push_step(&mut results, &template.template_str, step);
+        }
 
-    //     results
-    // }
+        results
+    }
 }
 
-// fn push_step(&mut self, template_str: &str, step: Step) {
-//     match step.kind {
-//         // steps
-//         StepKind::Tag => {
-//             push_element(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::ElementClosed => {
-//             close_element(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::VoidElementClosed => {
-//             close_void_element(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::Attr => {
-//             add_attr(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::AttrValue => {
-//             add_attr_value(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::AttrValueUnquoted => {
-//             add_attr_value_unquoted(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::Text => {
-//             push_text(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::TailTag => {
-//             pop_element(self, get_text_from_step(template_str, &step));
-//         }
-//         // injections
-//         StepKind::AttrMapInjection => {
-//             push_attr_map_injection(self, get_text_from_step(template_str, &step));
-//         }
-//         StepKind::DescendantInjection => {
-//             push_descendant_injection(self, get_text_from_step(template_str, &step));
-//         }
-//         // all other steps silently pass through
-//         _ => {}
-//     }
-// }
+fn push_step(results: &mut HtmlBuilderResults, template_str: &str, step: Step) {
+    match step.kind {
+        // // steps
+        // StepKind::Tag => {
+        //     push_element(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::ElementClosed => {
+        //     close_element(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::VoidElementClosed => {
+        //     close_void_element(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::Attr => {
+        //     add_attr(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::AttrValue => {
+        //     add_attr_value(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::AttrValueUnquoted => {
+        //     add_attr_value_unquoted(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::Text => {
+        //     push_text(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::TailTag => {
+        //     pop_element(self, get_text_from_step(template_str, &step));
+        // }
+        // // injections
+        // StepKind::AttrMapInjection => {
+        //     push_attr_map_injection(self, get_text_from_step(template_str, &step));
+        // }
+        // StepKind::DescendantInjection => {
+        //     push_descendant_injection(self, get_text_from_step(template_str, &step));
+        // }
+        // all other steps silently pass through
+        _ => {}
+    }
+}
 
 // push step
 // then get string
