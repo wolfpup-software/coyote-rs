38f51c8c41d909df6a4bcbec9600ee3d1b5ca353
Taylor Vann <brian.t.vann@gmail.com>
2024-06-02 14:00:06 -0700

    add stackbit enum and target templates

--- html/src/lib.rs
+++ html/src/lib.rs
@@ -2,16 +2,12 @@ use txml::{Component, Template};
 
 mod txml_builder;
 
-// need to render the template
-// then iterate through the built chunks
-
-struct TemplateBit<'a> {
-    pub template: &'a Template,
+struct TemplateBit {
     pub inj_index: usize,
 }
 
 pub enum StackBit<'a> {
-    Tmpl(TemplateBit<'a>),
+    Tmpl(&'a Component, TemplateBit),
     Cmpnt(&'a Component),
     None,
 }
@@ -21,57 +17,38 @@ fn getStackable(component: &Component) -> StackBit {
         Component::Text(text) => StackBit::Cmpnt(component),
         Component::List(list) => StackBit::Cmpnt(component),
         Component::Tmpl(tmpl) => {
-            return StackBit::Tmpl(TemplateBit {
-                template: tmpl,
-                inj_index: 0,
-            })
+            return StackBit::Tmpl(component, TemplateBit{inj_index: 0})
         }
         _ => return StackBit::None,
     }
 }
 
-// fn build_template(component: Component) -> String {
-//     let mut templ_str = "".to_string();
+fn build_template(component: Component) -> String {
+    let mut templ_str = "".to_string();
 
-//     let mut stack: Vec<Option<StackBit>> = Vec::from([getStackable(&component)]);
-//     while let Some(frame_opt) = stack.pop() {
-//         let mut stack_bit = match frame_opt {
-//             Some(frame) => frame,
-//             _ => continue,
-//         };
+    let mut stack: Vec<StackBit> = Vec::from([getStackable(&component)]);
+    while let Some(stack_bit) = stack.pop() {
+        // //new outer match grabing component or template
+        match stack_bit {
+            StackBit::Tmpl(tmpl, mut bit) => {
+                // figure out injection strategy
+                bit.inj_index += 1;
+            },
+            StackBit::Cmpnt(cmpnt) => {
+                match cmpnt {
+                    // break lists into smaller chuncks
+                    Component::List(list) => {
+                        for cmpnt in list.iter().rev() {
+                            stack.push(getStackable(cmpnt));
+                        }
+                    },
+                    Component::Text(text) => templ_str.push_str(text),
+                    _ => {},
+                }
+            },
+            _ => {}
+        }
+    }
 
-// new outer match grabing component or template
-
-//         match stack_bit.component {
-//             // break lists into smaller chuncks
-//             Component::List(list) => {
-//                 for cmpnt in list.iter().rev() {
-//                     stack.push(getStackable(cmpnt));
-//                 }
-//                 continue;
-//             },
-//             Component::Text(text) => templ_str.push_str(text),
-//             // if template
-//             _ => {},
-//         }
-//     }
-
-//     templ_str
-// }
-
-// fn add_text(templ: &mut String, text: &str) {
-//     templ.push_str(text);
-// }
-
-// // fn add_attr(templ: &mut String, attr: &str) {
-// //     templ.push_str(" ");
-// //     templ.push_str(attr);
-// // }
-
-// // fn add_attr_val(templ: &mut String, attr: &str, value: &str) {
-// //     templ.push_str(" ");
-// //     templ.push_str(attr);
-// //     templ.push_str("=\"");
-// //     templ.push_str(value);
-// //     templ.push_str("\"");
-// // }
+    templ_str
+}
