34fd2bc1bef9da9057b3045f962a1dfd2ee56b82
Taylor Vann <brian.t.vann@gmail.com>
2024-06-02 13:37:52 -0700

    stackbit enums

--- html/src/lib.rs
+++ html/src/lib.rs
@@ -1,69 +1,77 @@
-use txml::Component;
+use txml::{Component, Template};
 
 mod txml_builder;
 
 // need to render the template
 // then iterate through the built chunks
 
-struct StackBit<'a> {
-    pub component: &'a Component,
+struct TemplateBit<'a> {
+    pub template: &'a Template,
     pub inj_index: usize,
 }
 
-fn getStackable(component: &Component) -> Option<StackBit> {
-    let stackable = match component {
-        Component::Tmpl(tmpl) => component,
-        Component::Text(text) => component,
-        Component::List(list) => component,
-        _ => return None,
-    };
-
-    Some(StackBit {
-        component: stackable,
-        inj_index: 0,
-    })
+pub enum StackBit<'a> {
+    Tmpl(TemplateBit<'a>),
+    Cmpnt(&'a Component),
+    None,
 }
 
-fn build_template(component: Component) -> String {
-    let mut templ_str = "".to_string();
-
-    let mut stack: Vec<Option<StackBit>> = Vec::from([getStackable(&component)]);
-    while let Some(frame_opt) = stack.pop() {
-        let mut stack_bit = match frame_opt {
-            Some(frame) => frame,
-            _ => continue,
-        };
-
-        match stack_bit.component {
-            // break lists into smaller chuncks
-            Component::List(list) => {
-                for cmpnt in list.iter().rev() {
-                    stack.push(getStackable(cmpnt));
-                }
-                continue;
-            }
-            Component::Text(text) => templ_str.push_str(text),
-            // if template
-            _ => {}
+fn getStackable(component: &Component) -> StackBit {
+    match component {
+        Component::Text(text) => StackBit::Cmpnt(component),
+        Component::List(list) => StackBit::Cmpnt(component),
+        Component::Tmpl(tmpl) => {
+            return StackBit::Tmpl(TemplateBit {
+                template: tmpl,
+                inj_index: 0,
+            })
         }
+        _ => return StackBit::None,
     }
-
-    templ_str
 }
 
-fn add_text(templ: &mut String, text: &str) {
-    templ.push_str(text);
-}
+// fn build_template(component: Component) -> String {
+//     let mut templ_str = "".to_string();
 
-// fn add_attr(templ: &mut String, attr: &str) {
-//     templ.push_str(" ");
-//     templ.push_str(attr);
+//     let mut stack: Vec<Option<StackBit>> = Vec::from([getStackable(&component)]);
+//     while let Some(frame_opt) = stack.pop() {
+//         let mut stack_bit = match frame_opt {
+//             Some(frame) => frame,
+//             _ => continue,
+//         };
+
+// new outer match grabing component or template
+
+//         match stack_bit.component {
+//             // break lists into smaller chuncks
+//             Component::List(list) => {
+//                 for cmpnt in list.iter().rev() {
+//                     stack.push(getStackable(cmpnt));
+//                 }
+//                 continue;
+//             },
+//             Component::Text(text) => templ_str.push_str(text),
+//             // if template
+//             _ => {},
+//         }
+//     }
+
+//     templ_str
 // }
 
-// fn add_attr_val(templ: &mut String, attr: &str, value: &str) {
-//     templ.push_str(" ");
-//     templ.push_str(attr);
-//     templ.push_str("=\"");
-//     templ.push_str(value);
-//     templ.push_str("\"");
+// fn add_text(templ: &mut String, text: &str) {
+//     templ.push_str(text);
 // }
+
+// // fn add_attr(templ: &mut String, attr: &str) {
+// //     templ.push_str(" ");
+// //     templ.push_str(attr);
+// // }
+
+// // fn add_attr_val(templ: &mut String, attr: &str, value: &str) {
+// //     templ.push_str(" ");
+// //     templ.push_str(attr);
+// //     templ.push_str("=\"");
+// //     templ.push_str(value);
+// //     templ.push_str("\"");
+// // }
