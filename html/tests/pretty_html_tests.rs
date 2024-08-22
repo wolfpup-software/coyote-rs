35248547c2572fe5f233d59b4a35f72e2b3388ba
Taylor Vann <brian.t.vann@gmail.com>
2024-08-21 20:09:11 -0700

    uncomment tests

--- html/tests/pretty_html_tests.rs
+++ html/tests/pretty_html_tests.rs
@@ -90,98 +90,98 @@ fn test_pretty_html_doc() {
     assert_eq!(expected, results);
 }
 
-// #[test]
-// fn test_pretty_html_client() {
-//     let template = "        <!DOCTYPE>
-//     <html>
-//     <head>
-//         <style>
-// #woof .bark {
-// 	color: doggo;
-// }
-//         </style>
-//         <script>
-// if 2 < 3 {
-// 	console.log();
-// }
-//         </script>
-//     </head>
-//         <body>
-//             <article></article>
-//             <footer/>
-//         </body>
-// </html>";
-//     let expected =
-//         "<!DOCTYPE><html><head></head><body><article></article><footer></footer></body></html>";
+#[test]
+fn test_pretty_html_client() {
+    let template = "        <!DOCTYPE>
+    <html>
+    <head>
+        <style>
+#woof .bark {
+	color: doggo;
+}
+        </style>
+        <script>
+if 2 < 3 {
+	console.log();
+}
+        </script>
+    </head>
+        <body>
+            <article></article>
+            <footer/>
+        </body>
+</html>";
+    let expected =
+        "<!DOCTYPE><html><head></head><body><article></article><footer></footer></body></html>";
 
-//     let sieve = ClientSieve::new();
-//     let results = compose(&sieve, &template);
-//     assert_eq!(expected, results);
-// }
+    let sieve = ClientSieve::new();
+    let results = compose(&sieve, &template);
+    assert_eq!(expected, results);
+}
 
-// #[test]
-// fn test_pretty_html_web_component() {
-//     let template = "        <!DOCTYPE>
-//     <html>
-//     <head>
+#[test]
+fn test_pretty_html_web_component() {
+    let template = "        <!DOCTYPE>
+    <html>
+    <head>
 
-//     </head>
-//         <body>
-//             <article></article>
-//             <footer/>
-//         </body>
-// </html>";
+    </head>
+        <body>
+            <article></article>
+            <footer/>
+        </body>
+</html>";
 
-//     let expected =
-//         "<!DOCTYPE><html><head></head><body><article></article><footer></footer></body></html>";
+    let expected =
+        "<!DOCTYPE><html><head></head><body><article></article><footer></footer></body></html>";
 
-//     let sieve = ClientSieve::new();
-//     let results = compose(&sieve, &template);
-//     assert_eq!(expected, results);
-// }
+    let sieve = ClientSieve::new();
+    let results = compose(&sieve, &template);
+    assert_eq!(expected, results);
+}
 
-// #[test]
-// fn test_pretty_html_without_indents_server() {
-//     let template = "        <!DOCTYPE>
-//     <html>
-//     <head>
+#[test]
+fn test_pretty_html_without_indents_server() {
+    let template = "        <!DOCTYPE>
+    <html>
+    <head>
 
-//     </head>
-//         <body>
-//             <article>
-//                 You're a <span>boy kisser</span> aren't you?
-//                 Click <a>here</a> and go somewhere else.
-//             </article>
-//             <footer/>
-//         </body>
-// </html>";
+    </head>
+        <body>
+            <article>
+                You're a <span>boy kisser</span> aren't you?
+                Click <a>here</a> and go somewhere else.
+            </article>
+            <footer/>
+        </body>
+</html>";
 
-//     let expected =
-//     "<!DOCTYPE>\n<html>\n\t<head></head>\n\t<body>\n\t\t<article>\n\t\t\tYou're a <span>boy kisser</span> aren't you?\n\t\t\tClick <a>here</a> and go somewhere else.\n\t\t</article>\n\t\t<footer></footer>\n\t</body>\n</html>";
-//     let sieve = Sieve::new();
-//     let results = compose(&sieve, &template);
-//     assert_eq!(expected, results);
-// }
+    let expected =
+    "<!DOCTYPE>\n<html>\n\t<head></head>\n\t<body>\n\t\t<article>\n\t\t\tYou're a <span>boy kisser</span> aren't you?\n\t\t\tClick <a>here</a> and go somewhere else.\n\t\t</article>\n\t\t<footer></footer>\n\t</body>\n</html>";
+    let sieve = Sieve::new();
+    let results = compose(&sieve, &template);
+    assert_eq!(expected, results);
+}
 
-// #[test]
-// fn test_pretty_html_without_indents_client() {
-//     let template = "        <!DOCTYPE>
-//     <html>
-//     <head>
+#[test]
+fn test_pretty_html_without_indents_client() {
+    let template = "        <!DOCTYPE>
+    <html>
+    <head>
 
-//     </head>
-//         <body>
-//             <article>
-//                 You're a <span>boy kisser</span> aren't you?
-//                 Click <a>here</a> and go somewhere else.
-//             </article>
-//             <footer/>
-//         </body>
-// </html>";
+    </head>
+        <body>
+            <article>
+                You're a <span>boy kisser</span> aren't you?
+                Click <a>here</a> and go somewhere else.
+            </article>
+            <footer/>
+        </body>
+</html>";
 
-//     let expected =
-//         "<!DOCTYPE><html><head></head><body><article>You're a <span>boy kisser</span> aren't you? Click <a>here</a> and go somewhere else.</article><footer></footer></body></html>";
-//     let sieve = ClientSieve::new();
-//     let results = compose(&sieve, &template);
-//     assert_eq!(expected, results);
-// }
+    let expected =
+        "<!DOCTYPE><html><head></head><body><article>You're a <span>boy kisser</span> aren't you? Click <a>here</a> and go somewhere else.</article><footer></footer></body></html>";
+    let sieve = ClientSieve::new();
+    let results = compose(&sieve, &template);
+    assert_eq!(expected, results);
+}
